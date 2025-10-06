661d3d419829df86727b6d6836e21e5c55aa00f1
phuong2k4 <phuongsepfg@gmail.com>
2025-10-07 01:35:48 +0700

    heapsort test

--- c++testing.cpp
+++ c++testing.cpp
@@ -239,16 +239,44 @@ void quickSortforStr(string s[], int left , int right){
     if(right > i) quickSortforStr(s,i,right);
 }
 
+void heapify(int a[],int n, int i){
+    int largest = i;
+    int left = 2*i+1;
+    int right = 2*i+2;
+
+    if(left < n && a[left] > a[largest]){
+        largest = left;
+    }
+    if(right < n && a[right] > a[largest]){
+        largest = right;
+    }
+    if(largest != i){
+        swap(a[i], a[largest]);
+        heapify(a,n,largest);
+    }
+}
+
+void heapSort(int a[],int n){
+    //max heap
+    for(int i = n/2-1 ;i>=0;i--){
+        heapify(a,n,i);
+    }
+    for(int i = n-1;i>=1 ;i--){
+        swap(a[0],a[i]);
+        heapify(a,i,0);
+    }
+}
+
 int main()
 {
     int n;
     cin >> n;
-    string a[n];
+    int a[n];
     for (int i = 0; i < n; i++)
     {
         cin >> a[i];
     }
-    quickSortforStr(a, 0, n-1);
+    heapSort(a, n);
     for (int i = 0; i < n; i++)
     {
         cout << a[i] << " ";
